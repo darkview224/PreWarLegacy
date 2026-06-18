a3167709d8b1e46b415191038126679d04fcf07e
Andrew Marsden <awmarsden487@gmail.com>
2026-06-17 22:30:53 -0400

    Decks section is looking good and working well!

---
+++ PreWarLegacy.Frontend/app/components/deck.tsx
@@ -0,0 +1,21 @@
+interface Deck{
+    imagePath: string;
+    deckName: string;
+    decklists : string[];
+}
+
+export default function Deck({imagePath, deckName, decklists} : Deck){
+    return(
+        <div className="deckPanel">
+            <h1 className="deckName">{deckName}</h1>
+            <img src={imagePath}></img>
+            <div className="decklistPanel">
+                {decklists.map ((d, index) => 
+                    <div key={d}>
+                        <a href={d} target="_blank">Deck #{index + 1}</a>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
--- PreWarLegacy.Frontend/app/routes/home.tsx
+++ PreWarLegacy.Frontend/app/routes/home.tsx
@@ -2,18 +2,29 @@ import type { Route } from "./+types/home";
 import { useState } from "react";
 import { Link } from "react-router";
 import iconicCards from "/iconicCards.png"
+
 import Card from "../components/card"
-import SearchCards from "~/components/searchCards";
+import SearchCards from "../components/searchCards";
+import Deck from "../components/deck"
 
 const bannedCards = import.meta.glob<string>('../images/BannedCards/*.jpg', {
   import: 'default',
   eager: true,
-})
+});
+
+const deckImagesPath = import.meta.glob<string>('../images/DeckImages/*.jpg', {
+  import: 'default',
+  eager: true,
+});
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "PreWar Legacy" }
   ];
+}
+
+function getDeckImage(name : string){
+  return deckImagesPath["../images/DeckImages/" + name + ".jpg"];
 }
 
 export default function Home() {
@@ -104,116 +115,55 @@ export default function Home() {
         {active === "decks" && (
           <section id="decks" className="sectionPanel">
             <h2 className="sectionHeader">Decks</h2>
+            <p className="sectionText">
+              While it is impossible to capture all the decks that people play in Magic, this is a good collection of decks that were played in Legacy before War of the Spark was released. To get an idea of the metagame at the time, 
+              we can use the Wayback Machine to see <a href="https://web.archive.org/web/20190401023233/https://www.mtggoldfish.com/metagame/legacy#paper" target="_blank">MTG Goldfish's data from April 1, 2019</a>. We can also look 
+              at <a href="https://web.archive.org/web/20190413122147/http://www.mtgtop8.com/format?f=LE" target="_blank">MTG Top 8's data from April 13, 2019</a>. All the decklists belowhave been sourced from tournament results reported
+              around the time before War of the Spark was released. 
+            </p>
+            <br></br>
+            <p className="sectionText">
+              Note: Some decks from MTGO events may contain War of the Spark cards as it was released early on MTGO. 
+              <br></br>
+              <a href="https://www.mtggoldfish.com/tournament_searches/create?tournament_search%5Bname%5D=&tournament_search%5Bformat%5D=legacy&tournament_search%5Bdate_range%5D=01%2F01%2F2018+-+05%2F02%2F2019&commit=Search" target="_blank">MTG Goldfish Tournament Results</a>
+              <br></br>
+              <a href="https://mtgtop8.com/search" target="_blank">Search MTG Top 8</a> with the format set to "Legacy" and a date of "02/05/2019" in the "To" box.
+            </p>
+            <br></br>
 
-            {/* https://web.archive.org/web/20190401023233/https://www.mtggoldfish.com/metagame/legacy#paper */}
-
-            {/* https://www.mtggoldfish.com/tournament_searches/create?tournament_search%5Bname%5D=&tournament_search%5Bformat%5D=legacy&tournament_search%5Bdate_range%5D=01%2F01%2F2018+-+05%2F02%2F2019&commit=Search */}
-
-            dnt
-            https://www.mtggoldfish.com/deck/1831188#online
-            https://www.mtggoldfish.com/deck/1811498#online
-            https://mtgtop8.com/event?e=21720&d=346005&f=LE
-
-            grixis control
-            https://www.mtggoldfish.com/deck/1831202#online
-            https://www.mtggoldfish.com/deck/1831198#online
-            https://mtgtop8.com/event?e=21720&d=346004&f=LE
-
-            izzet delver
-            https://www.mtggoldfish.com/deck/1807549#online
-            https://mtgtop8.com/event?e=21754&d=346305&f=LE
-            https://mtgtop8.com/event?e=21680&d=345674&f=LE
-
-            mono red prison
-            https://www.mtggoldfish.com/deck/1811492#online
-            https://www.mtggoldfish.com/deck/1811490#online
-            https://mtgtop8.com/event?e=21701&d=345827&f=LE
-
-            sneak and show
-            https://www.mtggoldfish.com/deck/1811468#online
-            https://www.mtggoldfish.com/deck/1811497#online
-            https://mtgtop8.com/event?e=21711&d=345920&f=LE
-
-            miracles
-            https://www.mtggoldfish.com/deck/1811471#online
-            https://www.mtggoldfish.com/deck/1811486#online
-            https://mtgtop8.com/event?e=21720&d=346003&f=LE
-
-            grixis delver
-            https://www.mtggoldfish.com/deck/1811482#online
-            https://www.mtggoldfish.com/deck/1829368#online
-            hhttps://mtgtop8.com/event?e=21624&d=345187&f=LE
-
-            golgari depths
-            https://www.mtggoldfish.com/deck/1831206#online
-            https://www.mtggoldfish.com/deck/1769886#online
-            https://mtgtop8.com/event?e=21701&d=345822&f=LE
-
-            storm
-            https://www.mtggoldfish.com/deck/1811472#online
-            https://www.mtggoldfish.com/deck/1811494#online
-            https://mtgtop8.com/event?e=21754&d=346303&f=LE
-
-            stoneblade
-            https://www.mtggoldfish.com/deck/1811469#online
-            https://www.mtggoldfish.com/deck/1811474#online
-            https://mtgtop8.com/event?e=21711&d=345914&f=LE
-
-            reanimator
-            https://www.mtggoldfish.com/deck/1811470#online
-            https://www.mtggoldfish.com/deck/1831186#online
-            https://mtgtop8.com/event?e=21711&d=345915&f=LE
-
-            maverick
-            https://www.mtggoldfish.com/deck/1769914#online
-            https://www.mtggoldfish.com/deck/1831189#online
-            https://mtgtop8.com/event?e=21702&d=345831&f=LE
-
-            lands
-            https://www.mtggoldfish.com/deck/1831176#online
-            https://www.mtggoldfish.com/deck/1831195#online
-            https://mtgtop8.com/event?e=21711&d=345921&f=LE
-
-            deaths shadow
-            https://www.mtggoldfish.com/deck/1831196#online
-            https://mtgtop8.com/event?e=21701&d=345824&f=LE
-            https://mtgtop8.com/event?e=21754&d=346302&f=LE
-
-            delver variants
-            https://www.mtggoldfish.com/deck/1811493#online
-            https://mtgtop8.com/event?e=21754&d=346301&f=LE
-            https://mtgtop8.com/event?e=21658&d=345466&f=LE
-
-            4c loam
-            https://www.mtggoldfish.com/deck/1811500#online
-            https://mtgtop8.com/event?e=21702&d=345829&f=LE
-            https://mtgtop8.com/event?e=21676&d=345648&f=LE
-
-            elves
-            https://www.mtggoldfish.com/deck/1811487#online
-            https://www.mtggoldfish.com/deck/1831194#online
-            https://mtgtop8.com/event?e=21711&d=345918&f=LE
-
-            post/eldrazi
-            https://www.mtggoldfish.com/deck/1811503#online
-            https://www.mtggoldfish.com/deck/1811481#online
-            https://mtgtop8.com/event?e=21711&d=345916&f=LE
-
-            other
-            dredge: https://www.mtggoldfish.com/deck/1811483#online
-            burn: https://mtgtop8.com/event?e=21754&d=346306&f=LE
-            infect: https://mtgtop8.com/event?e=21399&d=343201&f=LE
-            painter: https://mtgtop8.com/event?e=21564&d=344657&f=LE
-            bomberman: https://mtgtop8.com/event?e=21470&d=343798&f=LE
-            Czech Pile: https://mtgtop8.com/event?e=21423&d=343421&f=LE
-            Affinity: https://mtgtop8.com/event?e=21381&d=343004&f=LE
-            Phoenix: https://mtgtop8.com/event?e=21381&d=343003&f=LE
-            food chain: https://mtgtop8.com/event?e=21384&d=343084&f=LE
-            tin fins: https://mtgtop8.com/event?e=21281&d=342301&f=LE
-            merfolk: https://mtgtop8.com/event?e=21281&d=342303&f=LE
-            enchantress: https://www.mtggoldfish.com/deck/1769884#online
-            Stryfo Pile: https://mtgtop8.com/event?e=21720&d=346002&f=LE
-
+            <div className="decksGrid">
+              <Deck imagePath={getDeckImage("dnt")} deckName="Death and Taxes" decklists={["https://www.mtggoldfish.com/deck/1831188#online", "https://www.mtggoldfish.com/deck/1811498#online", "https://mtgtop8.com/event?e=21720&d=346005&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("grixisControl")} deckName="Grixis Control" decklists={["https://www.mtggoldfish.com/deck/1831202#online", "https://www.mtggoldfish.com/deck/1831198#online", "https://mtgtop8.com/event?e=21720&d=346004&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("delver")} deckName="Izzet Delver" decklists={["https://www.mtggoldfish.com/deck/1807549#online", "https://mtgtop8.com/event?e=21754&d=346305&f=LE", "https://mtgtop8.com/event?e=21680&d=345674&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("monoRedPrison")} deckName="Mono Red Prison" decklists={["https://www.mtggoldfish.com/deck/1811492#online", "https://www.mtggoldfish.com/deck/1811490#online", "https://mtgtop8.com/event?e=21701&d=345827&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("sneakAndShow")} deckName="Sneak and Show" decklists={["https://www.mtggoldfish.com/deck/1811468#online", "https://www.mtggoldfish.com/deck/1811497#online", "https://mtgtop8.com/event?e=21711&d=345920&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("miracles")} deckName="Miracles" decklists={["https://www.mtggoldfish.com/deck/1811471#online", "https://www.mtggoldfish.com/deck/1811486#online", "https://mtgtop8.com/event?e=21720&d=346003&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("delver")} deckName="Grixis Delver" decklists={["https://www.mtggoldfish.com/deck/1811482#online", "https://www.mtggoldfish.com/deck/1829368#online", "https://mtgtop8.com/event?e=21624&d=345187&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("golgariDepths")} deckName="Golgari Depths" decklists={["https://www.mtggoldfish.com/deck/1831206#online", "https://www.mtggoldfish.com/deck/1769886#online", "https://mtgtop8.com/event?e=21701&d=345822&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("storm")} deckName="Storm" decklists={["https://www.mtggoldfish.com/deck/1811472#online", "https://www.mtggoldfish.com/deck/1811494#online", "https://mtgtop8.com/event?e=21754&d=346303&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("stoneblade")} deckName="Stoneblade" decklists={["https://www.mtggoldfish.com/deck/1811469#online", "https://www.mtggoldfish.com/deck/1811474#online", "https://mtgtop8.com/event?e=21711&d=345914&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("reanimator")} deckName="Reanimator" decklists={["https://www.mtggoldfish.com/deck/1811470#online", "https://www.mtggoldfish.com/deck/1831186#online", "https://mtgtop8.com/event?e=21711&d=345915&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("maverick")} deckName="Maverick" decklists={["https://www.mtggoldfish.com/deck/1769914#online", "https://www.mtggoldfish.com/deck/1831189#online", "https://mtgtop8.com/event?e=21702&d=345831&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("lands")} deckName="Lands" decklists={["https://www.mtggoldfish.com/deck/1831176#online", "https://www.mtggoldfish.com/deck/1831195#online", "https://mtgtop8.com/event?e=21711&d=345921&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("deathsShadow")} deckName="Death's Shadow" decklists={["https://www.mtggoldfish.com/deck/1831196#online", "https://mtgtop8.com/event?e=21701&d=345824&f=LE", "https://mtgtop8.com/event?e=21754&d=346302&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("delver")} deckName="Delver Variants" decklists={["https://www.mtggoldfish.com/deck/1811493#online", "https://mtgtop8.com/event?e=21754&d=346301&f=LE", "https://mtgtop8.com/event?e=21658&d=345466&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("4cLoam")} deckName="4C Loam" decklists={["https://www.mtggoldfish.com/deck/1811500#online", "https://mtgtop8.com/event?e=21702&d=345829&f=LE", "https://mtgtop8.com/event?e=21676&d=345648&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("elves")} deckName="Elves" decklists={["https://www.mtggoldfish.com/deck/1811487#online", "https://www.mtggoldfish.com/deck/1831194#online", "https://mtgtop8.com/event?e=21711&d=345918&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("postEldrazi")} deckName="Post/Eldrazi" decklists={["https://www.mtggoldfish.com/deck/1811503#online", "https://www.mtggoldfish.com/deck/1811481#online", "https://mtgtop8.com/event?e=21711&d=345916&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("dredge")} deckName="Dredge" decklists={["https://www.mtggoldfish.com/deck/1811483#online"]}></Deck>
+              <Deck imagePath={getDeckImage("burn")} deckName="Burn" decklists={["https://mtgtop8.com/event?e=21754&d=346306&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("infect")} deckName="Infect" decklists={["https://mtgtop8.com/event?e=21399&d=343201&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("painter")} deckName="Painter" decklists={["https://mtgtop8.com/event?e=21564&d=344657&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("bomberman")} deckName="Bomberman" decklists={["https://mtgtop8.com/event?e=21470&d=343798&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("czechPile")} deckName="Czech Pile" decklists={["https://mtgtop8.com/event?e=21423&d=343421&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("affinity")} deckName="Affinity" decklists={["https://mtgtop8.com/event?e=21381&d=343004&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("phoenix")} deckName="Phoenix" decklists={["https://mtgtop8.com/event?e=21381&d=343003&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("foodChain")} deckName="Food Chain" decklists={["https://mtgtop8.com/event?e=21384&d=343084&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("tinFins")} deckName="Tin Fins" decklists={["https://mtgtop8.com/event?e=21281&d=342301&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("merfolk")} deckName="Merfolk" decklists={["https://mtgtop8.com/event?e=21281&d=342303&f=LE"]}></Deck>
+              <Deck imagePath={getDeckImage("enchantress")} deckName="Enchantress" decklists={["https://www.mtggoldfish.com/deck/1769884#online"]}></Deck>
+              <Deck imagePath={getDeckImage("stryfoPile")} deckName="Stryfo Pile" decklists={["https://mtgtop8.com/event?e=21720&d=346002&f=LE"]}></Deck>
+            </div>
           </section>
         )}
 
