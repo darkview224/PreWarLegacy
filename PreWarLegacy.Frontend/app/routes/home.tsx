47a16aba4f04ce9b48e5b0f3530dc270ebac1340
Andrew Marsden <awmarsden487@gmail.com>
2026-06-20 17:47:41 -0400

    Changing About and Rules section

--- PreWarLegacy.Frontend/app/routes/home.tsx
+++ PreWarLegacy.Frontend/app/routes/home.tsx
@@ -19,7 +19,7 @@ const deckImagesPath = import.meta.glob<string>('../images/DeckImages/*.jpg', {
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "PreWar Legacy" }
+    { title: "PreWar" }
   ];
 }
 
@@ -39,7 +39,7 @@ export default function Home() {
   return (
     <main>
       <div className="navBar">
-        <Link to="/" className="logo">PreWar Legacy</Link>
+        <Link to="/" className="logo">PreWar</Link>
         <nav style={{display: "flex", gap: "50px"}}>
           <button onClick={() => handleNav("about")}>About</button>
           <button onClick={() => handleNav("rules")}>Rules</button>
@@ -59,22 +59,23 @@ export default function Home() {
 
             <h2 className="sectionHeader">About</h2>
             <p className="sectionText">
-              PreWar Legacy, or simply Prewar Magic, is a community-driven constructed Magic: The Gathering format including all cards which were tournament-legal prior to the release of the set War of the Spark (May 2019), 
-              except those banned in Legacy at that time. In June 2019, soon after the release of Modern Horizons, Wizards of the Coast announced a new design philosophy that started with War of the Spark
-              called <a href="https://magic.wizards.com/en/news/card-preview/fire-it-2019-06-21" target="_blank">FIRE</a>. This design philosophy has been prevalent in Magic since, and has drastically shaped the entire game. 
+              Prewar is a community-driven, constructed Magic: The Gathering format; capturing what many players feel was the high-water mark for gameplay. The format recreates Legacy in one of its most balanced yet dynamic states. 
+              Decks were powerful in a way that tended to reward rather than overwhelm skill. Combo, aggro, and control were all viable; games might take only two turns, or go on past twenty. Specialists could be rewarded for their intricate 
+              knowledge of a deck of choice, while generalists who could nail a metagame shift could steal events.  
             </p>
             <br></br>
             <p className="sectionText">
-              Legacy during the time period leading up to War of the Spark offered a unique gameplay experience. The format was both balanced and diverse, with no deck maintaining a prevalence over 10% for long, and dozens 
-              of contenders over 2%. Decks were powerful, but the format’s incredibly deep card pool and character allowed for a lot of success with brewing. This was evident in how the metagame and deck design shifted 
-              significantly from week-to-week for at least 3 seasons prior. The format was also extremely rewarding for skilled players, both generalists and deck specialists alike. Combo, aggro, and control were all viable, 
-              and games might take only two turns, or go on past twenty. To many, this was the game's high-water mark. Changes in design philosophy and sales strategy have made this experience impossible to recreate in contemporary Magic. 
-              PreWar Legacy exists because it gives that feeling, the highest of highs, and special play patterns with highly strategic decision making.
+              Prewar has dozens of viable deck types. While many decks had amazing longevity, It was rare for any one to be preeminent for long. Whether digging up overlooked and forgotten tech to get an edge with something classic, or 
+              brewing up something spicy and new, there’s immense room for innovation. With over 18,000 cards, the depth of the format offers practically limitless exploration. 
             </p>
             <br></br>
             <p className="sectionText">
-              With the explosion in popularity of <a href="https://magic.wizards.com/en/news/card-preview/fire-it-2019-06-21" target="_blank">Premodern</a>, it is clear that many players crave for an alternative style of competitive Magic: The Gathering.
-              Players are craving a format without FIRE design and Universes Beyond - even if it that means new cards will not be added. A closed format has now become a strength. Join the community today!
+              Prewar is also a closed format. Once seen as a liability, closed formats now offer a shield from the effects of <a href="https://magic.wizards.com/en/news/card-preview/fire-it-2019-06-21" target="_blank">FIRE Design</a> on gameplay 
+              and balance, and from the effects of Universes Beyond on game aesthetics and release schedules. As release schedules have made the idea of “non-rotating” formats illusory, a closed format delivers. 
+              With the explosion of <a href="https://magic.wizards.com/en/news/card-preview/fire-it-2019-06-21" target="_blank">Premodern</a>, it is clear that many players crave a classic Magic experience.
+            </p>
+            <p className="sectionText">
+              So does the idea of an extremely wide yet deep format, a massive card pool for brewing, intricate and complex gameplay, and a huge reward for skill appeal to you? If so, then join the PreWar community, and see if it’s for you!
             </p>
           </section>
         )}
@@ -83,15 +84,16 @@ export default function Home() {
           <section id="rules" className="sectionPanel">
             <h2 className="sectionHeader">Rules</h2>
             <p className="sectionText">
-              PreWar Legacy is played with modern Magic: The Gathering rules. All tournament legal versions of cards (and reprints) may be used in decks as long as that card was originally printed before War of the Spark's release on May 3, 2019. 
-              PreWar Legacy has the Legacy ban list that was present at the time of War of the Spark's release. 
+              PreWar is played with the current Magic: The Gathering tournament rules. Legacy decks may consist of cards from all Magic card sets released prior to War of the Spark; plus the following cards: Sewers of Estark, 
+              Windseeker Centaur, and Nalathni Dragon. This includes cards from expansions and special sets (like From the Vault, Magic: The Gathering — Commander, Duel Decks, Conspiracy, etc.). It does not include any cards with a 
+              first tournament legal printing in or after War of the Spark (WAR), nor does it include silver bordered cards. There is also a list of banned cards, which are not legal for play in the format.
             </p>
             <br></br>
             <p className="sectionText">
               The following cards are banned:
             </p>
             <ul className="list-disc pl-6 sectionText" style={{textAlign: "left", marginLeft: "50px"}}>
-              <li>Cards with a name originally printed in the War of the Spark expansion, or any set released at a later date (May 3, 2019).</li>
+              <li>Cards released at a date later than May 2, 2019.</li>
               <li>25 cards with the Card Type <a href="https://scryfall.com/search?q=type%3Aconspiracy" target="_blank">Conspiracy</a>.</li>
               <li>9 cards that reference "playing for <a href="https://scryfall.com/search?q=o%3A%22+ante%22" target="_blank">ante</a>.</li>
             </ul>
